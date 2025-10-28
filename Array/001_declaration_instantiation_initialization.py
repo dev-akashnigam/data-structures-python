73936dbc7f8c13a78e1e65e934913bc97131611c
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-28 11:19:30 +0530

    Creating an array - Declaration, Instantiation & Initialization

---
+++ Array/001_declaration_instantiation_initialization.py
@@ -0,0 +1,13 @@
+import array
+
+# Declaration (declaration only)
+
+arr1 = None
+
+# Instantiation (declaration and instantiation)
+
+arr2 = array.array('i', [0]*5)
+
+# Initialization (declaration, instantiation and initialization)
+
+arr3 = array.array('i', [10, 20, 30, 40, 50])
