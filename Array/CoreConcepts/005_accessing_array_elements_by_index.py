32b9696b50db448813e679be8dcd9bb0111f2aa5
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-28 12:37:04 +0530

    Accessing/Reading array elements by index

---
+++ Array/CoreConcepts/005_accessing_array_elements_by_index.py
@@ -0,0 +1,11 @@
+import array
+
+arr = array.array('i', [10, 20, 30, 40, 50])
+
+first_element = arr[0]
+forth_element = arr[3]
+last_element = arr[-1]
+
+print(f"First element of array = {first_element}")
+print(f"Forth element of array = {forth_element}")
+print(f"Last element of array = {last_element}")
