20ae6d289e165735e36a16a7d58f42140051f265
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-03 17:26:48 +0530

    Printing Entire Array

--- Array/CoreConcepts/027_reducing_array_to_a_single_value.py
+++ Array/CoreConcepts/027_reducing_array_to_a_single_value.py
@@ -1,21 +1,23 @@
 import array
 from functools import reduce
 
-def fn_sum(element_1, element_2):
-    return element_1 + element_2
+def fn_sum(element_1, element_2):  
+    return element_1 + element_2  #the sum returned becomes the first argument in the next iteration i.e element_1 while the next array element becomes element_2.
 
 def fn_mul(element_1, element_2):
-    return element_1 * element_2
+    return element_1 * element_2 #the product returned becomes the first argument in the next iteration i.e element_1 while the next array element becomes element_2.
 
 def fn_max(element_1, element_2):
     if element_1 > element_2:
-        return element_1
+        return element_1 
     else:
         return element_2
+#the element returned (element_1 or element_2) becomes element_1 in the next iteration while the next array element becomes element_2.
+
 
 arr = array.array('i', [10, 20, 30, 40, 50])
 
-sum_of_all_array_elements = reduce(fn_sum, arr)
+sum_of_all_array_elements = reduce(fn_sum, arr, sum, 0)
 print(f"Sum of all array elements = {sum_of_all_array_elements}")
 
 product_of_all_array_elements = reduce(fn_mul, arr)
---
+++ Array/CoreConcepts/028_printing_array_elements.py
@@ -0,0 +1,5 @@
+import array
+
+arr = array.array('i', [10, 20, 30, 40, 50])
+
+print(f"On printing using print(f\" \"), arr = {arr}")
