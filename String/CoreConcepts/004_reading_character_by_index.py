c1c4162bd79622a02c48ff2a3abcef20b4b162df
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-06 13:21:31 +0530

    Reading any character of string by index

---
+++ String/CoreConcepts/004_reading_character_by_index.py
@@ -0,0 +1,10 @@
+str = "Hello World"
+
+first_char = str[0]
+print(f"First character in string: {str} = {first_char}")
+
+fifth_char = str[4]
+print(f"Fifth character in string: {str} = {fifth_char}")
+
+last_char = str[-1]
+print(f"Last character in string: {str} = {last_char}")
