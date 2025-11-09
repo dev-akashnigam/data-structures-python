6b393087e383863d36d39958eb2456b274e9a162
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-09 12:57:31 +0530

    Determine if a given string starts with a given substring.

---
+++ String/CoreConcepts/008_determine_if_string_starts_with_substring.py
@@ -0,0 +1,8 @@
+str = "This story is about Akash Nigam"
+print(f"Original String = {str}")
+
+subs = "This"
+
+start_check = str.startswith(subs)
+
+print(f"String: \"{str}\" starts with substring: \"{subs}\" is = {start_check}")
