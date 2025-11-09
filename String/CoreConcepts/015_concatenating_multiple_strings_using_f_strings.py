6b3032e16f75e8d63c9d5186f0162eaa5026ec4e
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-10 01:34:31 +0530

    Concatenating multiple strings using f-string

---
+++ String/CoreConcepts/015_concatenating_multiple_strings_using_f_strings.py
@@ -0,0 +1,11 @@
+str_1 = "Hello"
+print(f"First String = {str_1}")
+
+str_2 = "Beautiful"
+print(f"Second String = {str_2}")
+
+str_3 = "World"
+print(f"Third String = {str_3}")
+
+concatenated_string = f"{str_1}{str_2}{str_3}"
+print(f"Concatenated String = {concatenated_string}")
