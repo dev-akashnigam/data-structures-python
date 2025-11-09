25c798ef638f8a14e27bcf123b3b8a8a4fcd8b7b
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-10 00:43:13 +0530

    Copying substring from a given index to another given index in a string

---
+++ String/CoreConcepts/013_copy_substring_by_indices.py
@@ -0,0 +1,8 @@
+str = "He accidentally joined a yoga class thinking it was a sandwich shop"
+print(f"Original string = {str}")
+
+copy_from_index = 3
+copy_to_index = 11
+
+subs = str[copy_from_index:copy_to_index]
+print(f"Substring from index: {copy_from_index} to index: {copy_to_index} = {subs}")
