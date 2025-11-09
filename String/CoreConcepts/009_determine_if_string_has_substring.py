3dbb8da95f22dbf6faff430fe7fb26a9f06ecd50
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-09 13:19:10 +0530

    Determine if a given string has a given substring.

---
+++ String/CoreConcepts/009_determine_if_string_has_substring.py
@@ -0,0 +1,8 @@
+str = "Hey boys, I am singing a song. Soup song. Flop song. Why this kolaveri kolaveri kolaveri d"
+print(f"Original String = {str}")
+
+subs = "Flop"
+
+has_check = subs in str
+
+print(f"String: \"{str}\" has substring \"{subs}\" = {has_check}")
