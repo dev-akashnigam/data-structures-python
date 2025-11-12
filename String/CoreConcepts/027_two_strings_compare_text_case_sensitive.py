1bb1aaee4667765f09f6ab1de59cf8120ecf5866
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-13 00:18:45 +0530

    Comparing text of two different strings.

---
+++ String/CoreConcepts/027_two_strings_compare_text_case_sensitive.py
@@ -0,0 +1,13 @@
+str_1 = "Life moves fast"
+print(f"First string = {str_1}")
+
+str_2 = "Life moves fast"
+print(f"Second string = {str_2}")
+
+are_text_same = str_1 == str_2
+
+if are_text_same:
+    print(f"Both the strings have same text.")
+else:
+    print(f"Both the strings have different text.")
+
