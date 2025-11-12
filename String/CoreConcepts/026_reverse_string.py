0b70edcfadf8fe5e7ab693d1f5afd914bb5d4f79
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-13 00:01:39 +0530

    Reversing a given string

---
+++ String/CoreConcepts/026_reverse_string.py
@@ -0,0 +1,5 @@
+str = "Please review documents"
+print(f"Original String = {str}")
+
+str = str[::-1] # syntax - str[start:end:step], when start index and end index are not provided, they are assumed to be 0 and index of the last array element.
+print(f"Reversed String = {str}")
