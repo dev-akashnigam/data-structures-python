0d1fada8559a2d2c87d0cf8240b847dae2321a47
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-12 13:08:36 +0530

    Determining count/frequency of occurence of a substring in a given string

---
+++ String/CoreConcepts/025_frequency_of_occurence_of_substring.py
@@ -0,0 +1,7 @@
+str = "He fixed the issue, logged the issue, and explained the issue during the meeting."
+print(f"Original String = {str}")
+
+subs = "issue"
+
+count_of_occurence_of_subs = str.count(subs)
+print(f"Count of occurence of substring: \"{subs}\" in the string = {count_of_occurence_of_subs}")
