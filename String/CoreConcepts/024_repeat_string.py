6377a39a21650e9694ed800f5d8983acb13a571f
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-12 12:23:54 +0530

    Repeating a given string N times.

---
+++ String/CoreConcepts/024_repeat_string.py
@@ -0,0 +1,7 @@
+str = "Meeting starts soon."
+print(f"Original String = {str}")
+
+repeat_count = 3
+str = str * repeat_count
+print(f"After repeating string {repeat_count} times, string = {str}")
+
